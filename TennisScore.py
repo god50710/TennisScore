225eec3b75db8dee012b4ac2f2757ed00dfebead
Kouitsuhou <god50710@gmail.com>
2018-04-07 17:53:42 +0800

    Revert "fix type and pass test : fifteen love"
    This reverts commit 9107d68262053c802a7454c9223db94983941b7f.

--- TennisScore.py
+++ TennisScore.py
@@ -1,10 +1,7 @@
-SCORE_LIST = ["Love", "Fifteen", "Thirty", "Forty"]
+SCORE_LIST = ["All", "Fifteen", "Thirty", "Forty"]
 
 
 class TennisScore:
     @classmethod
     def tennis_judge(cls, player_1, player_2, player_1_score, player_2_score):
-        if player_1_score != player_2_score:
-            return SCORE_LIST[player_1_score] + " " + SCORE_LIST[player_2_score]
-        else:
-            return "Love All"
+        return "Love " + SCORE_LIST[player_2_score]
