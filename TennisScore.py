4ef3e8f088ea4f120eed1975625e9520c9b80c68
Kouitsuhou <god50710@gmail.com>
2018-04-07 22:37:44 +0800

    refactor all tennis judge flow

--- TennisScore.py
+++ TennisScore.py
@@ -5,25 +5,44 @@ ACTION_LIST = [" Advance", " Win"]
 class TennisScore:
     @classmethod
     def tennis_judge(cls, game):
-        if game.player_1_score != game.player_2_score:
-            if game.player_1_score < 4 and game.player_2_score < 4:
-                return SCORE_LIST[game.player_1_score] + " " + SCORE_LIST[game.player_2_score]
-            else:
-                return cls.lead_player_name(game) + ACTION_LIST[cls.game_action(game)]
-        elif game.player_1_score >= 3:
-            return "Deuce"
-        else:
-            return SCORE_LIST[game.player_1_score] + " All"
+        return cls.show_same_score(game) if cls.is_same_score(game) else \
+            cls.leader_action(game) if cls.is_ready_to_win(game) else cls.show_score(game)
 
     @classmethod
-    def lead_player_name(cls, game):
-        return game.player_2_name if cls.player_2_has_higher_score(game) else game.player_1_name
+    def show_score(cls, game):
+        return SCORE_LIST[game.player_1_score] + " " + SCORE_LIST[game.player_2_score]
 
     @classmethod
-    def game_action(cls, game):
+    def leader_action(cls, game):
+        return cls.leader_name(game) + ACTION_LIST[cls.judge_adv_or_win(game)]
+
+    @classmethod
+    def leader_name(cls, game):
+        return game.player_1_name if cls.player_1_has_higher_score(game) else game.player_2_name
+
+    @classmethod
+    def judge_adv_or_win(cls, game):
         return abs(game.player_1_score - game.player_2_score) >> 1
 
     @classmethod
-    def player_2_has_higher_score(cls, game):
-        return game.player_2_score > game.player_1_score
+    def player_1_has_higher_score(cls, game):
+        return game.player_1_score > game.player_2_score
+
+    @classmethod
+    def is_same_score(cls, game):
+        return game.player_1_score == game.player_2_score
+
+    @classmethod
+    def is_deuce(cls, game):
+        return game.player_1_score >= 3
+
+    @classmethod
+    def is_ready_to_win(cls, game):
+        return game.player_1_score >= 4 or game.player_2_score >= 4
+
+    @classmethod
+    def show_same_score(cls, game):
+        return "Deuce" if cls.is_deuce(game) else SCORE_LIST[game.player_1_score] + " All"
+
+
 
--- test_tennisScore.py
+++ test_tennisScore.py
@@ -6,43 +6,43 @@ from TennisScore import TennisScore
 
 class TestTennisScore(TestCase):
     def test_love_all(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 0, 0)), "Love All")
+        self.assertEqual("Love All", TennisScore.tennis_judge(Game("Eric", "Maru", 0, 0)))
 
     def test_love_fifteen(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 0, 1)), "Love Fifteen")
+        self.assertEqual("Love Fifteen", TennisScore.tennis_judge(Game("Eric", "Maru", 0, 1)))
 
     def test_love_thirty(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 0, 2)), "Love Thirty")
+        self.assertEqual("Love Thirty", TennisScore.tennis_judge(Game("Eric", "Maru", 0, 2)))
 
     def test_love_forty(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 0, 3)), "Love Forty")
+        self.assertEqual("Love Forty", TennisScore.tennis_judge(Game("Eric", "Maru", 0, 3)))
 
     def test_fifteen_love(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 1, 0)), "Fifteen Love")
+        self.assertEqual("Fifteen Love", TennisScore.tennis_judge(Game("Eric", "Maru", 1, 0)))
 
     def test_thirty_love(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 2, 0)), "Thirty Love")
+        self.assertEqual("Thirty Love", TennisScore.tennis_judge(Game("Eric", "Maru", 2, 0)))
 
     def test_fifteen_all(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 1, 1)), "Fifteen All")
+        self.assertEqual("Fifteen All", TennisScore.tennis_judge(Game("Eric", "Maru", 1, 1)))
 
     def test_thirty_all(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 2, 2)), "Thirty All")
+        self.assertEqual("Thirty All", TennisScore.tennis_judge(Game("Eric", "Maru", 2, 2)))
 
     def test_deuce_3_vs_3(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 3, 3)), "Deuce")
+        self.assertEqual("Deuce", TennisScore.tennis_judge(Game("Eric", "Maru", 3, 3)))
 
     def test_deuce_4_vs_4(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 4, 4)), "Deuce")
+        self.assertEqual( "Deuce", TennisScore.tennis_judge(Game("Eric", "Maru", 4, 4)))
 
     def test_first_player_adv(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 4, 3)), "Eric Advance")
+        self.assertEqual("Eric Advance", TennisScore.tennis_judge(Game("Eric", "Maru", 4, 3)))
 
     def test_second_player_adv(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 3, 4)), "Maru Advance")
+        self.assertEqual("Maru Advance", TennisScore.tennis_judge(Game("Eric", "Maru", 3, 4)))
 
     def test_second_player_win_with_5_point(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 3, 5)), "Maru Win")
+        self.assertEqual("Maru Win", TennisScore.tennis_judge(Game("Eric", "Maru", 3, 5)))
 
     def test_first_player_win_with_6_point(self):
-        self.assertEqual(TennisScore.tennis_judge(Game("Eric", "Maru", 6, 4)), "Eric Win")
+        self.assertEqual("Eric Win",TennisScore.tennis_judge(Game("Eric", "Maru", 6, 4)))
