95627d723aece8142b0bef2361a0272a1c1ebc5f
Kouitsuhou <god50710@gmail.com>
2018-04-07 18:11:24 +0800

    pass test : deuce_4_vs_4

--- TennisScore.py
+++ TennisScore.py
@@ -6,7 +6,7 @@ class TennisScore:
     def tennis_judge(cls, player_1, player_2, player_1_score, player_2_score):
         if player_1_score != player_2_score:
             return SCORE_LIST[player_1_score] + " " + SCORE_LIST[player_2_score]
-        elif player_1_score == 3:
+        elif player_1_score >= 3:
             return "Deuce"
         else:
             return SCORE_LIST[player_1_score] + " All"
