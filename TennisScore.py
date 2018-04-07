ec815c2ad943a75ebfb8400f9d7efdf69d4c3fce
Kouitsuhou <god50710@gmail.com>
2018-04-07 19:58:07 +0800

    pass test : first_player_adv

--- TennisScore.py
+++ TennisScore.py
@@ -3,9 +3,12 @@ SCORE_LIST = ["Love", "Fifteen", "Thirty", "Forty"]
 
 class TennisScore:
     @classmethod
-    def tennis_judge(cls, player_1, player_2, player_1_score, player_2_score):
+    def tennis_judge(cls, player_1_name, player_2_name, player_1_score, player_2_score):
         if player_1_score != player_2_score:
-            return SCORE_LIST[player_1_score] + " " + SCORE_LIST[player_2_score]
+            if player_1_score > player_2_score and player_1_score > 3:
+                return player_1_name + " Advance"
+            else:
+                return SCORE_LIST[player_1_score] + " " + SCORE_LIST[player_2_score]
         elif player_1_score >= 3:
             return "Deuce"
         else:
