8a5fac96f24218e1573449e7c0db86d6afe3415d
Kouitsuhou <god50710@gmail.com>
2018-04-07 17:42:06 +0800

    pass test : love fifteen

--- TennisScore.py
+++ TennisScore.py
@@ -1,4 +1,7 @@
 class TennisScore:
     @classmethod
     def tennis_judge(cls, player_1, player_2, player_1_score, player_2_score):
-        return "Love All"
+        if player_2_score == 1:
+            return "Love Fifteen"
+        else:
+            return "Love All"
