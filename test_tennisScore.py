db8aa883a6c7f2e1bc76ec1743ac40a96f951b11
Kouitsuhou <god50710@gmail.com>
2018-04-07 17:41:01 +0800

    add test case : love fifteen

--- test_tennisScore.py
+++ test_tennisScore.py
@@ -6,3 +6,6 @@ class TestTennisScore(TestCase):
     def test_love_all(self):
         self.assertEqual(TennisScore.tennis_judge("Eric", "Maru", 0, 0), "Love All")
 
+    def test_love_Fifteen(self):
+        self.assertEqual(TennisScore.tennis_judge("Eric", "Maru", 0, 1), "Love Fifteen")
+
