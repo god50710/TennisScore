b15656ace9166689b4d031878dbd85ac96e8f673
Kouitsuhou <god50710@gmail.com>
2018-04-07 20:08:02 +0800

    pass test : first player win with 6 point

--- TennisScore.py
+++ TennisScore.py
@@ -6,7 +6,10 @@ class TennisScore:
     def tennis_judge(cls, player_1_name, player_2_name, player_1_score, player_2_score):
         if player_1_score != player_2_score:
             if player_1_score > player_2_score and player_1_score > 3:
-                return player_1_name + " Advance"
+                if player_1_score - player_2_score > 1:
+                    return player_1_name + " Win"
+                else:
+                    return player_1_name + " Advance"
             elif player_2_score > player_1_score and player_2_score > 3:
                 if player_2_score - player_1_score > 1:
                     return player_2_name + " Win"
